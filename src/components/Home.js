801ddd7048557d16bd745fd0f7a3e23e3ba1414b
rtv313isc <rtv313.isc@hotmail.com>
2021-04-13 21:18:50 -0500

    set title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
 import '../App.css';
+import React from 'react';
 import {Link} from 'react-router-dom'
 import Logout from './Logout'
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 
 class Home extends React.Component {
@@ -15,11 +18,14 @@ class Home extends React.Component {
     
     return (
       <div className="App">
-        <Logout/>
-        <Breadcrumbs>
-          <Link to="/users"><h1>Users</h1></Link>
-          <Link to="/exercises"><h1>Exercises</h1></Link>
-        </Breadcrumbs>
+        <CssBaseline />
+        <Container maxWidth="md">
+          <Logout/>
+          <Breadcrumbs>
+            <Link to="/users"><h1>Users</h1></Link>
+            <Link to="/exercises"><h1>Exercises</h1></Link>
+          </Breadcrumbs>
+        </Container>
       </div>
     );
   }
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,8 +2,19 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+      marginLeft: theme.spacing(3),
+      marginRight: theme.spacing(1),
+    },
+  }));
+
 
 const Logout = () => {
+
+    const classes = useStyles();
 
     const logout = () => {
         sessionStorage.removeItem("accessToken");
@@ -14,9 +25,10 @@ const Logout = () => {
   return (
     <AppBar position="static" color="default">
         <Toolbar>
-            <Button variant="contained" color="primary"  onClick={logout}  >
+            <Button variant="contained" color="primary"  onClick={logout}>
                 Logout
             </Button>
+            <h2 className={classes.title}> Gym App Admin UI</h2>
         </Toolbar>
       </AppBar>
   );
