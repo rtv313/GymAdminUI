55f59f6c7636dfe40a513ccc2e1d8f32741aab64
rtv313isc <rtv313.isc@hotmail.com>
2021-04-13 20:44:37 -0500

    enable login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/Menu';
+import Login from './components/Login';
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Menu/>
+          <Login/>
       </div>
     );
   }
