fa78bd823f9c819fba1657021d115f51404f5c34
rtv313isc <rtv313.isc@hotmail.com>
2021-04-13 21:59:54 -0500

    Better home menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,55 @@
-import '../App.css';
-import React from 'react';
-import {Link} from 'react-router-dom'
-import Logout from './Logout'
-import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-
+import "../App.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Logout from "./Logout";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
 
 class Home extends React.Component {
-
   constructor(props) {
     super(props);
   }
 
-  render(){
-    
+  render() {
     return (
       <div className="App">
         <CssBaseline />
         <Container maxWidth="md">
-          <Logout/>
-          <Breadcrumbs>
-            <Link to="/users"><h1>Users</h1></Link>
-            <Link to="/exercises"><h1>Exercises</h1></Link>
-          </Breadcrumbs>
+          <Logout />
+          <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                App Modules
+              </ListSubheader>
+            }
+          >
+            <Link to="/users">
+              <ListItem button>
+                <ListItemIcon>
+                  <AccountBoxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Users" />
+              </ListItem>
+            </Link>
+
+            <Link to="/exercises">
+              <ListItem button>
+                <ListItemIcon>
+                  <DirectionsRunIcon />
+                </ListItemIcon>
+                <ListItemText primary="Exercises" />
+              </ListItem>
+            </Link>
+          </List>
         </Container>
       </div>
     );
