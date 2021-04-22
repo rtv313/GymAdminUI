c69d0757e9824acd3725054482216930d149d79f
rtv313isc <rtv313.isc@hotmail.com>
2021-04-21 20:58:56 -0500

    basic example of rendering

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -11,8 +11,46 @@ import Container from "@material-ui/core/Container";
 import Button from '@material-ui/core/Button';
 import Logout from "./Logout";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { DataGrid } from "@material-ui/data-grid";
+
+
+
+
 
 class Exercises extends React.Component {
+
+  handleClick = () => {
+    return alert("Hola mundo");
+  }
+
+   columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "firstName", headerName: "First name", width: 130 },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "age",headerName: "Age",type: "number",width: 90},
+    { field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>`${params.getValue("firstName") || ""} ${params.getValue("lastName") || ""}`,
+      renderCell: (params) => {
+        return <Button variant="contained" color="primary" onClick={this.handleClick}>Click {params.getValue("firstName")}</Button>;
+      }
+    }
+  ];
+  
+   rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 }
+  ];
 
   constructor(props) {
     super(props);
@@ -67,8 +105,9 @@ class Exercises extends React.Component {
       Header: 'Edit',
       sortable: false,
       filterable: false,
-      width: 100, 
-      Cell: ({value, row}) => (<p>Edit</p>),
+      width: 100,
+      accessor: 'id', 
+      Cell: ({value, row}) => (<p>Edit {value}</p>),
     }, {
       Header: 'Delete',
       sortable: false,
@@ -94,9 +133,20 @@ class Exercises extends React.Component {
         </Breadcrumbs>
 
         <h1>Exercises</h1>
-          <ReactTable data={this.state.exercises} columns={columns} filterable={true}/>
-          <ToastContainer autoClose={1500} /> 
+          <ReactTable data={this.state.exercises} columns={columns} filterable={true}/> 
+
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid rows={this.rows} columns={this.columns} pageSize={5} 
+            filterModel={{
+              items: [
+                { columnField: 'lastName', operatorValue: 'contains', value: 'Snow' },
+              ],
+            }}/>
+          </div>
         </Container>
+        <br/>
+
+      
       </div>
       
     );
