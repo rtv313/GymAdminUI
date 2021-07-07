d369844f3bd79427abfec1363d2dbec4dbc39af7
rtv313isc <rtv313.isc@hotmail.com>
2021-07-06 20:43:25 -0500

    get subscriptions

--- src/components/Subscriptions.js
+++ src/components/Subscriptions.js
@@ -10,26 +10,81 @@ import Button from "@material-ui/core/Button";
 import Logout from "./Logout";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { DataGrid } from "@material-ui/data-grid";
-import AddExercise from "./AddExercise";
+
 
 
 class Subscriptions extends React.Component {
   
+  columnsSubscriptions = [
+    { field: "normalUser", headerName: "User", width: 230 },
+    { field: "coachUser", headerName: "coach", width: 230 },
+    {
+      field: "delete",
+      disableClickEventBubbling: true,
+      headerName: "Delete",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      renderCell: (params) => {
+
+        const onClick = () => {
+          this.deleteExercise(params.getValue("id"));
+         };
+   
+        return (
+          <Button variant="contained" color="secondary" onClick={onClick}>
+            Delete
+          </Button>
+        );
+      },
+    },
+  ];
+
+  setNormalUser = (value) => {
+    value.normalUser = value.normalUser.email;
+    return value;
+  };
+
+   setCoachUser = (value) => {
+    value.coachUser = value.coachUser.email;
+    return value;
+  };
 
   constructor(props) {
     super(props);
-    this.state = { exercises: [] };
+    this.state = { subscriptions: [] };
   }
 
   componentDidMount() {
-  
+    this.fetchSubscriptions();
   }
 
  
+  fetchSubscriptions = () => {
+    // Read the token from the session storage
+    // and include it to Authorization header
+    const token = "Bearer " + sessionStorage.getItem("accessToken");
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", token);
 
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(SERVER_URL + "api/subscriptions/", requestOptions)
+      .then((response) => response.json())
+      .then((responseData) => {
+        responseData.map(this.setNormalUser);
+        responseData.map(this.setCoachUser);
+        this.setState({
+          subscriptions: responseData,
+        });
+      })
+      .catch((err) => console.error(err));
+  }
   
-
-
   render() {
     return (
       <div>
@@ -46,8 +101,11 @@ class Subscriptions extends React.Component {
           </Breadcrumbs>
           <h1>Subscriptions</h1>
           <div style={{ height: 400, width: "100%" }}>
-            
-           
+          <DataGrid
+            rows={this.state.subscriptions}
+            columns={this.columnsSubscriptions}
+            pageSize={50}
+          />
           </div>
         </Container>
         <br />
